71fba5a533d88b5c2e2f00bcde2d6b00d1629fce
LoganX07 <asimx07@gmail.com>
2019-11-21 18:03:37 +0500

    Full Backend

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,4 +43,99 @@ router.post("/register", (req, res) => {
     .catch(err => console.log(err));
 });
 
+// @route   GET api/profile/all/
+// @desc    Get profile by email
+// @access  Public
+
+router.get("/all", (req, res) => {
+  const errors = {};
+  profile
+    .find()
+    .populate("user", ["name"])
+    .then(Profile => {
+      if (!Profile) {
+        errors.noProfile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(Profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/email/:email
+// @desc    Get profile by email
+// @access  Public
+
+router.get("/email/:email", (req, res) => {
+  const errors = {};
+
+  profile
+    .findOne({ email: req.params.email })
+    .populate("user", ["name"])
+    .then(Profile => {
+      if (!Profile) {
+        errors.noProfile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(Profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   PUT api/profile/update/:email
+// @desc    update profile by email
+// @access  Public
+
+router.put("/update/:email", (req, res) => {
+  let updatedUser = {
+    name: req.body.name,
+    contact: req.body.contact,
+    address: req.body.address,
+    age: req.body.age
+  };
+
+  profile
+    .findOne({ email: req.params.email })
+    .then(Profile => {
+      if (Profile) {
+        // Update
+        profile
+          .findOneAndUpdate(
+            { email: req.params.email },
+            { $set: updatedUser },
+            { new: true }
+          )
+          .then(Profile => res.json(Profile))
+          .catch(err => req.json({ msg: "Dusra hai" + err }));
+      }
+    })
+    .catch(err => req.json({ msg: "pehla hai" + err }));
+});
+
+// @route   DELETE api/profile/email/:email_id
+// @desc    Delete User
+// @access  Public
+router.delete("/deleteuser/:email", (req, res) => {
+  profile
+    .findOneAndDelete(req.params.email)
+    .then(result => {
+      res.json({
+        success: true,
+        msg: `It has been deleted.`,
+        result: {
+          _id: result._id,
+          name: result.name,
+          email: result.email,
+          age: result.age,
+          gender: result.gender
+        }
+      });
+    })
+    .catch(err => {
+      res.status(404).json({ success: false, msg: "Nothing to delete." });
+    });
+});
+
 module.exports = router;
